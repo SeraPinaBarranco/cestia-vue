45f8bbe338cc3ff560b26cd8b016c6f4a9e5da4d
SerafinPinaBarranco <arespin23@gmail.com>
2026-04-20 13:00:07 +0200

    correccion de imagen

--- cestia-vue/src/components/BotonEscanearComponent.vue
+++ cestia-vue/src/components/BotonEscanearComponent.vue
@@ -5,7 +5,7 @@
   <div class="boton-escanear-container">
     <button class="btn-escanear" @click="escanearTicket">Escanear ticket</button>
   </div>
-  <img ref="imgRef" :src="imagenCapturada" alt="Imagen capturada" v-if="imagenCapturada" />
+  <img :src="imagenCapturada" v-if="imagenCapturada" />
   <p v-if="textoOCR">{{ textoOCR }}</p>
 
 </template>
@@ -20,24 +20,12 @@ const canvasRef = ref(null)
 const imagenCapturada = ref(null)
 const textoOCR = ref('')
 
-async function procesarOCR(imagen) {
-  const resultado = await Tesseract.recognize(imagen, 'spa')
-  textoOCR.value = resultado.data.text
-  textoOCR.value = limpiarTexto(resultado.data.text)
-
-}
-
-
 async function escanearTicket() {
-  //alert('Escaneando ticket...');
   const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
   videoRef.value.srcObject = stream
-
-  // Espera a que el vídeo esté listo antes de capturar
   videoRef.value.onloadedmetadata = async () => {
     await capturarFotograma()
   }
-
 }
 
 async function capturarFotograma() {
@@ -47,19 +35,15 @@ async function capturarFotograma() {
   canvas.width = video.videoWidth
   canvas.height = video.videoHeight
 
-  canvas.getContext('2d').drawImage(video, 0, 0)
+  const ctx = canvas.getContext('2d')
+  ctx.drawImage(video, 0, 0)
+  ctx.filter = 'contrast(150%) grayscale(100%)'
+  ctx.drawImage(canvas, 0, 0)
 
-  // return canvas.toDataURL('image/png')
   imagenCapturada.value = canvas.toDataURL('image/png')
 
-  await procesarOCR(imagenCapturada.value)
-}
-
-function limpiarTexto(texto) {
-  return texto
-    .replace(/O/g, '0')         // letra O confundida con cero
-    .replace(/l(?=\d)/g, '1')   // letra l delante de número
-    .replace(/(\d),(\d)/g, '$1.$2') // coma decimal → punto
+  const resultado = await Tesseract.recognize(imagenCapturada.value, 'spa')
+  textoOCR.value = resultado.data.text
 }
 
 
@@ -89,10 +73,6 @@ function limpiarTexto(texto) {
   border-radius: 5px;
   font-weight: bold;
 
-}
-
-.input-imagen-ticket {
-  display: none;
 }
 
 .btn-escanear:hover {
